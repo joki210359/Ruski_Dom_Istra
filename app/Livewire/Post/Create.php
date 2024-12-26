a871bbab7a456aade4cad9d894e082a95a86b132
joki210359 <joki2103@outlook..com>
2024-12-26 22:37:01 +0100

    19-Design post upload layout

--- app/Livewire/Post/Create.php
+++ app/Livewire/Post/Create.php
@@ -4,8 +4,17 @@ namespace App\Livewire\Post;
 
 use Livewire\Component;
 use LivewireUI\Modal\ModalComponent;
+
 class Create extends ModalComponent
 {
+    /**
+     * Supported: 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
+     */
+    public static function modalMaxWidth(): string
+    {
+        return '4xl';
+    }
+
     public function render()
     {
         return view('livewire.post.create');
